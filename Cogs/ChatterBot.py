ea51658ed01c3820a9f31b9c55e053708ea0bc6e
Kein Pyi Si <keinpyisi@gmail.com>
2022-05-26 03:45:46 +0900

    MIKU

--- Cogs/ChatterBot.py
+++ Cogs/ChatterBot.py
@@ -50,13 +50,13 @@ class ChatterBot(commands.Cog):
 		# Check if we can chat
 		lastTime = int(self.settings.getServerStat(server, "LastChat", 0))
 		threshold = int(self.waitTime)
-		currentTime = int(time.time())
+		currentTime = int(time.perf_counter())
 
 		if currentTime < (int(lastTime) + int(threshold)):
 			return False
 		
 		# If we made it here - set the LastPicture method
-		self.settings.setServerStat(server, "LastChat", int(time.time()))
+		self.settings.setServerStat(server, "LastChat", int(time.perf_counter()))
 		return True
 	
 	async def killcheck(self, message):
